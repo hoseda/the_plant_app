dcd699ef39667dcf4fb3dcacb06b5dbbded44e1f
husayn <65117441+hoseda@users.noreply.github.com>
2025-12-25 12:56:57 +0330

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,10 +19,6 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
-}
-
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
